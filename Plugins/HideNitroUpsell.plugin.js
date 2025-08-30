78957771d649d245a00ef205499c0cab5bbb8f29
rauko <32078834+rauko23385@users.noreply.github.com>
2025-08-30 09:56:36 +0100

    Update HideNitroUpsell.plugin.js

--- Plugins/HideNitroUpsell.plugin.js
+++ Plugins/HideNitroUpsell.plugin.js
@@ -1,14 +1,7 @@
 /**
- * @name Hide Nitro Upsell
+ * @name Hide Nitro Upsell from chat boxes
  * @description Hides all upsell of Nitro in common used UI across the Discord.
- * @author Sam @yungsamd17
  * @version 1.1
- * @authorId 402083911053869056
- * @invite GYveRfruk6
- * @donate https://ko-fi.com/yungsamd17
- * @website https://yungsamd17.github.io/
- * @source https://github.com/yungsamd17/BetterDiscordAddons/tree/main/Plugins/HideNitroUpsell
- * @updateUrl https://raw.githubusercontent.com/yungsamd17/BetterDiscordAddons/main/Plugins/HideNitroUpsell/HideNitroUpsell.plugin.js
 */
 
 module.exports = () => ({
@@ -22,34 +15,11 @@ module.exports = () => ({
         link.style.display = 'none';
       });
 
-      // Super Reaction in message right click context menu
-      const reactionDiv = document.getElementById('message-add-reaction-1');
-      if (reactionDiv) {
-        reactionDiv.style.display = 'none';
-      }
-
       // Gift button in message input box
       const giftButton = document.querySelector('button[aria-label="Send a gift"]');
       if (giftButton) {
         giftButton.style.display = 'none';
-      }
-
-      // Profile decoration banner for Nitro Shop in Profile settings
-      const premiumBannerDivs = document.querySelectorAll('[class*="premiumFeatureBannerBackground-"]');
-      premiumBannerDivs.forEach(div => {
-        div.style.display = 'none';
-      })
-
-      // Small "Add Super Reaction" button on right of the message when hovering over it
-      const smallReactionDiv = document.querySelectorAll('[aria-label="Add Super Reaction"]');
-      smallReactionDiv.forEach(div => {
-        div.style.display = 'none';
-      })
-      // Super Reaction tab in Reactions menu 'picker'
-      const superReactionTab = document.getElementById('super-reaction-picker-tab');
-      if (superReactionTab) {
-        superReactionTab.style.display = 'none';
-      };
+      }};
     }
 
     // Function to observe DOM mutations and reapply the hiding logic
